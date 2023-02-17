06c828df261bde94de37d064bbaa0e04e613186d
James Jenkins <James.Jenkins@ibm.com>
2023-02-17 09:01:03 -0500

    Set network DNS
    Configure the DNS server for new public networks.

--- test-containerd/instantiate_vm_and_test.sh
+++ test-containerd/instantiate_vm_and_test.sh
@@ -69,7 +69,7 @@ NAME="$NAME-$RAND_VAL"
 
 # Create public network for VM
 NETNAME="prow-net-$RAND_VAL"
-NETWORK=$(ibmcloud pi netcpu $NETNAME | grep -m 1 ID | awk '{print $2}') || true
+NETWORK=$(ibmcloud pi netcpu $NETNAME --dns-servers "9.9.9.9" | grep -m 1 ID | awk '{print $2}') || true
 
 if [ -z "$NETWORK" ]; then echo "FAIL: fail to configure network."; exit 1; fi
 
