AshwinHIBM/docker-ce-build

Use 0.7-ppc64le for static builds

build-docker.sh
@@ -234,7 +234,7 @@ echo "docker run -d \
            ${DOCKER_SECRET_AUTH_IN_ENV} \
            --privileged \
            --name ${CONT_NAME} \
-           quay.io/${QUAYIO_REPOSITORY}/docker-ce-build \
+           quay.io/${QUAYIO_REPOSITORY}/docker-ce-build@${DIND_QUAY_STATIC_HASH} \
            ${PATH_SCRIPTS}/build-static.sh"
            
 docker run -d \
@@ -245,7 +245,7 @@ docker run -d \
            ${DOCKER_SECRET_AUTH_IN_ENV} \
            --privileged \
            --name ${CONT_NAME} \
-           quay.io/${QUAYIO_REPOSITORY}/docker-ce-build \
+           quay.io/${QUAYIO_REPOSITORY}/docker-ce-build@${DIND_QUAY_STATIC_HASH} \
            ${PATH_SCRIPTS}/build-static.sh
 
 status_code="$(docker container wait ${CONT_NAME})"

get-env.sh
@@ -70,12 +70,12 @@ then
     echo "The env.list has not been generated."
     exit 1
 else
-# Check there are 8 env variables in env.list from github
-    if grep -Fq "DOCKER_TAG" ${FILE_ENV} && grep -Fq "DOCKER_CLI_HASH" && grep -Fq "DOCKER_ENGINE_HASH" ${FILE_ENV} && grep -Fq "DOCKER_PACKAGING_HASH" ${FILE_ENV} && grep -Fq "CONTAINERD_BUILD" ${FILE_ENV} && grep -Fq "CONTAINERD_HASH" ${FILE_ENV} && grep -Fq "CONTAINERD_TAG" ${FILE_ENV} && grep -Fq "CONTAINERD_PACKAGING_HASH" ${FILE_ENV} && grep -Fq "RUNC_VERS" ${FILE_ENV}
+# Check there are 9 env variables in env.list from github
+    if grep -Fq "DOCKER_TAG" ${FILE_ENV} && grep -Fq "DOCKER_CLI_HASH" && grep -Fq "DOCKER_ENGINE_HASH" ${FILE_ENV} && grep -Fq "DOCKER_PACKAGING_HASH" ${FILE_ENV} && grep -Fq "CONTAINERD_BUILD" ${FILE_ENV} && grep -Fq "CONTAINERD_HASH" ${FILE_ENV} && grep -Fq "CONTAINERD_TAG" ${FILE_ENV} && grep -Fq "CONTAINERD_PACKAGING_HASH" ${FILE_ENV} && grep -Fq "RUNC_VERS" ${FILE_ENV} && grep -Fq "DIND_QUAY_STATIC_HASH" ${FILE_ENV}
     then
-        echo "DOCKER_TAG : ${DOCKER_TAG}, DOCKER_CLI_HASH : ${DOCKER_CLI_HASH}, DOCKER_ENGINE_HASH : ${DOCKER_ENGINE_HASH}, DOCKER_PACKAGING_HASH : ${DOCKER_PACKAGING_HASH}, CONTAINERD_BUILD : ${CONTAINERD_BUILD}, CONTAINERD_HASH : ${CONTAINERD_HASH}, CONTAINERD_TAG : ${CONTAINERD_TAG}, CONTAINERD_PACKAGING_HASH : ${CONTAINERD_PACKAGING_HASH} and RUNC_VERS :${RUNC_VERS} are in env.list."
+        echo "DOCKER_TAG : ${DOCKER_TAG}, DOCKER_CLI_HASH : ${DOCKER_CLI_HASH}, DOCKER_ENGINE_HASH : ${DOCKER_ENGINE_HASH}, DOCKER_PACKAGING_HASH : ${DOCKER_PACKAGING_HASH}, CONTAINERD_BUILD : ${CONTAINERD_BUILD}, CONTAINERD_HASH : ${CONTAINERD_HASH}, CONTAINERD_TAG : ${CONTAINERD_TAG}, CONTAINERD_PACKAGING_HASH : ${CONTAINERD_PACKAGING_HASH}, RUNC_VERS :${RUNC_VERS} and ${DIND_QUAY_STATIC_HASH} are in env.list."
     else
-        echo "DOCKER_TAG, DOCKER_CLI_HASH, DOCKER_ENGINE_HASH, DOCKER_PACKAGING_HASH, CONTAINERD_BUILD, CONTAINERD_HASH, CONTAINERD_TAG, CONTAINERD_PACKAGING_HASH and/or RUNC_VERS are not in env.list."
+        echo "DOCKER_TAG, DOCKER_CLI_HASH, DOCKER_ENGINE_HASH, DOCKER_PACKAGING_HASH, CONTAINERD_BUILD, CONTAINERD_HASH, CONTAINERD_TAG, CONTAINERD_PACKAGING_HASH, RUNC_VERS and/or DIND_QUAY_STATIC_HASH are not in env.list."
         cat /workspace/${FILE_ENV}
         exit 1
     fi

ppc64le-cloud/test

build-docker.sh
@@ -234,7 +234,7 @@ echo "docker run -d \
            ${DOCKER_SECRET_AUTH_IN_ENV} \
            --privileged \
            --name ${CONT_NAME} \
-           quay.io/${QUAYIO_REPOSITORY}/docker-ce-build \
+           quay.io/${QUAYIO_REPOSITORY}/docker-ce-build@${DIND_IMG_STATIC_HASH} \
            ${PATH_SCRIPTS}/build-static.sh"
            
 docker run -d \
@@ -245,7 +245,7 @@ docker run -d \
            ${DOCKER_SECRET_AUTH_IN_ENV} \
            --privileged \
            --name ${CONT_NAME} \
-           quay.io/${QUAYIO_REPOSITORY}/docker-ce-build \
+           quay.io/${QUAYIO_REPOSITORY}/docker-ce-build@${DIND_IMG_STATIC_HASH} \
            ${PATH_SCRIPTS}/build-static.sh
 
 status_code="$(docker container wait ${CONT_NAME})"

get-env.sh
@@ -70,12 +70,12 @@ then
     echo "The env.list has not been generated."
     exit 1
 else
-# Check there are 8 env variables in env.list from github
-    if grep -Fq "DOCKER_TAG" ${FILE_ENV} && grep -Fq "DOCKER_CLI_HASH" && grep -Fq "DOCKER_ENGINE_HASH" ${FILE_ENV} && grep -Fq "DOCKER_PACKAGING_HASH" ${FILE_ENV} && grep -Fq "CONTAINERD_BUILD" ${FILE_ENV} && grep -Fq "CONTAINERD_HASH" ${FILE_ENV} && grep -Fq "CONTAINERD_TAG" ${FILE_ENV} && grep -Fq "CONTAINERD_PACKAGING_HASH" ${FILE_ENV} && grep -Fq "RUNC_VERS" ${FILE_ENV}
+# Check there are 9 env variables in env.list from github
+    if grep -Fq "DOCKER_TAG" ${FILE_ENV} && grep -Fq "DOCKER_CLI_HASH" && grep -Fq "DOCKER_ENGINE_HASH" ${FILE_ENV} && grep -Fq "DOCKER_PACKAGING_HASH" ${FILE_ENV} && grep -Fq "CONTAINERD_BUILD" ${FILE_ENV} && grep -Fq "CONTAINERD_HASH" ${FILE_ENV} && grep -Fq "CONTAINERD_TAG" ${FILE_ENV} && grep -Fq "CONTAINERD_PACKAGING_HASH" ${FILE_ENV} && grep -Fq "RUNC_VERS" ${FILE_ENV} && grep -Fq "DIND_IMG_STATIC_HASH" ${FILE_ENV}
     then
-        echo "DOCKER_TAG : ${DOCKER_TAG}, DOCKER_CLI_HASH : ${DOCKER_CLI_HASH}, DOCKER_ENGINE_HASH : ${DOCKER_ENGINE_HASH}, DOCKER_PACKAGING_HASH : ${DOCKER_PACKAGING_HASH}, CONTAINERD_BUILD : ${CONTAINERD_BUILD}, CONTAINERD_HASH : ${CONTAINERD_HASH}, CONTAINERD_TAG : ${CONTAINERD_TAG}, CONTAINERD_PACKAGING_HASH : ${CONTAINERD_PACKAGING_HASH} and RUNC_VERS :${RUNC_VERS} are in env.list."
+        echo "DOCKER_TAG : ${DOCKER_TAG}, DOCKER_CLI_HASH : ${DOCKER_CLI_HASH}, DOCKER_ENGINE_HASH : ${DOCKER_ENGINE_HASH}, DOCKER_PACKAGING_HASH : ${DOCKER_PACKAGING_HASH}, CONTAINERD_BUILD : ${CONTAINERD_BUILD}, CONTAINERD_HASH : ${CONTAINERD_HASH}, CONTAINERD_TAG : ${CONTAINERD_TAG}, CONTAINERD_PACKAGING_HASH : ${CONTAINERD_PACKAGING_HASH}, RUNC_VERS :${RUNC_VERS} and ${DIND_IMG_STATIC_HASH} are in env.list."
     else
-        echo "DOCKER_TAG, DOCKER_CLI_HASH, DOCKER_ENGINE_HASH, DOCKER_PACKAGING_HASH, CONTAINERD_BUILD, CONTAINERD_HASH, CONTAINERD_TAG, CONTAINERD_PACKAGING_HASH and/or RUNC_VERS are not in env.list."
+        echo "DOCKER_TAG, DOCKER_CLI_HASH, DOCKER_ENGINE_HASH, DOCKER_PACKAGING_HASH, CONTAINERD_BUILD, CONTAINERD_HASH, CONTAINERD_TAG, CONTAINERD_PACKAGING_HASH, RUNC_VERS and/or DIND_IMG_STATIC_HASH are not in env.list."
         cat /workspace/${FILE_ENV}
         exit 1
     fi