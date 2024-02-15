d1f3b5e6f884e18d0be4f5f2f6fc8e7d76b40ae4
Ashwin Hendre <ashwin.hendre2@ibm.com>
2024-02-15 12:37:38 +0530

    Integration tests

--- upstream-master-ci/prow-integration-tests.sh
+++ upstream-master-ci/prow-integration-tests.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -u
 
-echo "Prow Job to run CI tests on the Docker packages"
+begin=$SECONDS
+
+echo "Prow Job to run integration tests on the Docker packages"
 
 ${PWD}/dockerctl.sh start
 
@@ -14,5 +16,20 @@ chmod ug+x ${PATH_CI}/get-env-ci.sh && ${PATH_CI}/get-env-ci.sh
 echo "*** Build dev image ***"
 chmod ug+x ${PATH_CI}/build-dev-image.sh && ${PATH_CI}/build-dev-image.sh
 
+exit_code_build=$?
+echo "Exit code build : ${exit_code_build}"
+
+if [[ $? != 0 ]]; then
+    echo "Building dev image failed."
+    exit 1
+fi
+
 echo "*** Run integration tests ***"
 chmod ug+x ${PATH_CI}/integration-tests.sh && ${PATH_CI}/integration-tests.sh
+
+exit_code_integration=$?
+echo "Exit code integration tests : ${exit_code_integration}"
+
+duration=$(( $SECONDS - $begin ))
+echo "DURATION INTEGRATION TEST $(( $duration / 60 )) minutes and $(( $duration % 60 )) seconds elapsed." 
+exit $exit_code_integration
