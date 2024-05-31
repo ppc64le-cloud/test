ab46a7dae3759eb779bc7fcd6f41f248838eb746
Ashwin Hendre <ashwin.hendre2@ibm.com>
2024-05-31 15:12:54 +0530

    Store CI logs

--- upstream-master-ci/build-dev-image.sh
+++ upstream-master-ci/build-dev-image.sh
@@ -1,4 +1,27 @@
 pushd ${PWD}/moby
-docker buildx build --load --force-rm -t "docker-dev" .
+
+# Store logs by commit id to avoid overwriting
+export COMMIT=$(git rev-parse --short HEAD)
+
+checkDirectory() {
+  if ! test -d $1
+  then
+    mkdir $1
+    if [[ $? -ne 0 ]]; then
+      echo "Could not create $1, exiting."
+      exit 1
+    fi
+    echo "$1 created"
+  else
+    echo "$1 already exists, will not create again."
+  fi
+}
+
+DIR_LOGS_COS="/mnt/s3_ppc64le-docker/prow-docker/ppc64le-ci/${COMMIT}"
+checkDirectory ${DIR_LOGS_COS}
+
+rm -f ${DIR_LOGS_COS}/build-dev-image.log && touch ${DIR_LOGS_COS}/build-dev-image.log
+docker buildx build --load --force-rm -t "docker-dev" . 2>&1 | tee -a "${DIR_LOGS_COS}/build-dev-image.log"
+
 popd
 exit 0
